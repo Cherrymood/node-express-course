8d4a2c29aac66cda7561e6e7425b65815493e3bd
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2024-12-10 23:07:19 -0800

    Added CRUD

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,7 +1,19 @@
 import express from "express";
+import tasks from "./routes/tasks.js";
+import { clientMongo } from "./db/connectMongo.js";
 
 const app = express();
 const port = 3000;
+
+// middleware
+app.use(express.json());
+app.use(express.static("./public"));
+
+await clientMongo.connect();
+//routes
+app.use("/api/v1/tasks", tasks);
+
+await clientMongo.close();
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
