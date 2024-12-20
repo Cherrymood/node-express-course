4754033f0999919effdccacbf4670fbdc1831161
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2024-12-20 14:24:17 -0800

    Added numericFilters

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -6,7 +6,7 @@ async function getAllProductsStatic(req, res) {
 }
 
 async function getAllProducts(req, res) {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -24,6 +24,27 @@ async function getAllProducts(req, res) {
     result = result.sort(sortParams);
   } else {
     result = result.sort("createAt");
+  }
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+    const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ["price", "rating"];
+    filters = filters.split(",").forEach((item) => {
+      const [field, operator, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [operator]: Number(value) };
+      }
+    });
   }
 
   if (fields) {
