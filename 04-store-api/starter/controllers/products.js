649611c1e1a4ab5bd05b417e8b829798c02d9ac5
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2024-12-20 14:20:51 -0800

    Added fields, page, limits and skip queries

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -6,7 +6,7 @@ async function getAllProductsStatic(req, res) {
 }
 
 async function getAllProducts(req, res) {
-  const { featured, company, name, sort } = req.query;
+  const { featured, company, name, sort, fields } = req.query;
   const queryObject = {};
   if (featured) {
     queryObject.featured = featured === "true" ? true : false;
@@ -25,6 +25,18 @@ async function getAllProducts(req, res) {
   } else {
     result = result.sort("createAt");
   }
+
+  if (fields) {
+    const fieldsParams = fields.split(",").join(" "); //parse incoming qParams for using at server
+    result = result.select(fieldsParams);
+  }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
+
   const products = await result;
 
   res.status(200).json({ products, nbHits: products.length });
