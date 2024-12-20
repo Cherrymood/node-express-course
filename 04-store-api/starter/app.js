68639e44253b1827dbf7422eed9a3aaa6b95c34e
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2024-12-20 10:39:14 -0800

    Set up server

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,1 +1,33 @@
-console.log('04 Store API')
+import express from "express";
+import env from "dotenv";
+import notFound from "./middleware/not-found.js";
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+
+const app = express();
+const port = 3000;
+env.config();
+
+// middleware
+app.use(express.json());
+app.use(express.static("./public"));
+
+//routes
+app.get("/", (req, res) => {
+  res.send(
+    '<h1> Store API </h1><a href="/api/v1/products"> products route</a>'
+  );
+});
+
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+async function Start() {
+  try {
+    //connect DB
+    app.listen(port, console.log(`Server is listening port ${3000}`));
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+Start();
