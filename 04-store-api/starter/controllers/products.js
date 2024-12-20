5c89e1ddc052fe5ff6f0b3509accdff1984f176c
Valeriia Horodnycha <valeriia.horodnycha@gmail.com>
2024-12-20 14:05:00 -0800

    Added search and sort queries

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,9 +1,33 @@
+import Product from "../models/product.js";
+
 async function getAllProductsStatic(req, res) {
-  res.status(200).json({ msg: "products testing route" });
+  const products = await Product.find({ name: "vase table" });
+  res.status(200).json({ products, nbHits: products.length });
 }
 
 async function getAllProducts(req, res) {
-  res.status(200).json({ msg: "products route" });
+  const { featured, company, name, sort } = req.query;
+  const queryObject = {};
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
+  }
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  let result = Product.find(queryObject);
+
+  if (sort) {
+    const sortParams = sort.split(",").join(" "); //parse incoming qParams for using at server
+    result = result.sort(sortParams);
+  } else {
+    result = result.sort("createAt");
+  }
+  const products = await result;
+
+  res.status(200).json({ products, nbHits: products.length });
 }
 
 export { getAllProducts, getAllProductsStatic };
